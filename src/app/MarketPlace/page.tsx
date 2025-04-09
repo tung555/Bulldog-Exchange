ae39508c586a463057ca819c6120677ac58d1d17
sts99340 <123031585+sts99340@users.noreply.github.com>
2025-04-09 16:30:29 -0400

    Add files via upload

---
+++ src/app/MarketPlace/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { useState } from 'react';
+import Navbar from '@/components/Navbar';
+
+export default function SignupPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  //i will implement later
+  const handleSignup = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Signing up with:', { name, email, password });
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="min-h-screen bg-white flex justify-center px-4">
+        <div className="flex justify-center w-full max-w-md p-6 bg-white rounded-lg mt-10">
+            <h1 className="text-3xl font-bold text-red-500 mb-6 text-center">
+                BULLDOG MARKETPLACE
+            </h1>
+        </div>
+      </div>
+    </>
+  );
+}
