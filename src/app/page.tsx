6d089ef7c6fa812666d57904fa96eeaa7a239ce9
KevoBro <91642406+KevoBro@users.noreply.github.com>
2025-04-09 18:56:59 -0400

    Added styling to button and subtext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,21 +19,21 @@ export default function Home() {
           priority
         />
         {/* Container for title */}
-        <div className="absolute inset-0 flex justify-center text-white top-10">
-          <h1 className="text-4xl md:text-6xl font-bold text-center px-3 py-2 rounded-sm"
-          style={{ backgroundColor: 'rgba(241, 45, 45, 1)', border:'1px', height: 'fit-content', WebkitTextStroke: '2px black' }}>
+        <div className="absolute inset-0 flex justify-center text-white top-8">
+          <h1 className="text-4xl md:text-6xl font-bold text-center px-3 py-2 rounded-sm coiny-regular "
+          style={{ backgroundColor: 'rgba(241, 45, 45, 1)', border:'5px solid #cc020c', height: 'fit-content', WebkitTextStroke: '2px black'}}>
             Welcome to Bulldog Exchange
           </h1>
         </div> 
-        <div className="absolute inset-0 flex justify-center text-white top-30">
-          <h2 className="text-2xl md:text-4xl font-bold text-center py-2 rounded-sm px-3"
-          style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)', border:'1px', height: 'fit-content', WebkitTextStroke: '1px black', fontSize:'30px'}}>
+        <div className="absolute inset-0 flex justify-center text-white top-30" >
+          <h2 className="text-2xl md:text-4xl font-bold text-center py-2 rounded-sm px-3 shadow-md"
+          style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)', border:'3px solid #fcfafa', height: 'fit-content', WebkitTextStroke: '1px black', fontSize:'30px'}}>
             Sign in to join other UGA Dawgs and sell your used books and items today!
           </h2>
         </div>
         <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2">
           <Link href="/MarketPlace">
-            <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded shadow-lg border-1 border-white" style={{WebkitTextStroke: '1px black'}}>
               Browse Now
             </button>
           </Link>
