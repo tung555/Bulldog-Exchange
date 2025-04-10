473fdf564652a9c3fd945c3e2efcf46bf4ef6309
evan <evga0904@gmail.com>
2025-04-09 21:20:05 -0400

    Added component to group listings

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,25 +1,28 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import textbooks from '../../public/textbooks.jpg'
 import { useState } from 'react';
 
-const Listing = () => {
+interface ListingData {
+    listing : {
+        id: number;
+        title: string;
+        price: number;
+        description: string;  
+        image: string  
+    }
+}
 
-    const [listingData, setListingData] = useState(
-        {
-            title: "Title",
-            price: "Price",
-            description: "Description"
-        }
-    );
+const Listing = ({listing}:ListingData) => {
+
 
     return (
-        <div className=" flex-col bg-red-500 h-90 min-w-2xs max-w-2xs rounded-md m-20 border-2">
+        <div className="flex-col bg-red-500 h-90 min-w-2xs max-w-2xs rounded-md m-20 border-2">
             <Image className = "h-3/5 w-full rounded-sm" src={textbooks} alt = "Used textbooks"/>
             <div className='w-full h-2/5 p-[6px]'>
                 <div className='bg-white w-full h-full rounded-md text-left p-[5px]'>
-                    <p className='text-lg'>{listingData.title}</p>
-                    <p className='text-md mb-[5px]'>{listingData.price}</p>
-                    <p className='text-sm'>{listingData.description}</p>
+                    <p className='text-lg'>{listing.title}</p>
+                    <p className='text-md mb-[5px]'>${listing.price}</p>
+                    <p className='text-sm'>{listing.description}</p>
                 </div>
             </div>
         </div>
