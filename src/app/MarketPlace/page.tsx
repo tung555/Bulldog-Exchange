021c30b8c0d56ab934c041dc92dccc7ae8ad9d5a
sts99340 <123031585+sts99340@users.noreply.github.com>
2025-04-12 17:45:29 -0400

    Update page.tsx

--- src/app/MarketPlace/page.tsx
+++ src/app/MarketPlace/page.tsx
@@ -1,24 +1,31 @@
 'use client';
 import { useState } from 'react';
 import Navbar from '@/components/Navbar';
-import Listing from '@/components/Listing'
-import Listings from '@/components/Listings'
+import Listing from '@/components/Listing';
+import Listings from '@/components/Listings';
+import Footer from '@/components/footer';
 
 export default function MarketPlace() {
-
   return (
     <>
       <Navbar />
 
-      <div className="min-h-screen bg-white flex-col justify-between px-4">
-        <div className="flex justify-center w-full max-w-md p-6 bg-white rounded-lg mt-10">
-            <h1 className="text-3xl font-bold text-red-500 mb-6 text-center">
-                BULLDOG MARKETPLACE
-            </h1>
-                       
+      <div className="min-h-screen bg-white flex flex-col items-center px-4">
+        <div className="flex flex-col w-full max-w-md p-6 bg-white rounded-lg">
+          <h1 className="text-3xl font-bold text-red-500 mb-6 text-center">
+            BULLDOG MARKETPLACE
+          </h1>
+          <div className="search-bar mb-4 flex justify-center">
+            <input
+              type="text"
+              placeholder="Search for items by type or title..."
+              className="w-full p-2 rounded-lg border border-gray-300 text-black"
+            />
+          </div>
         </div>
-        <Listings/> 
+        <Listings />
       </div>
+      <Footer />
     </>
   );
 }
